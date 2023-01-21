2046ebba6bab54ca50fa2a7bfde887c752883dc9
kirti pandey <pkirti497@gmail.com>
2023-01-21 22:15:07 +0530

    hello this is my first commit

---
+++ gallery.php
@@ -0,0 +1,374 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Gallery</title>
+   
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="style.css">
+   <style>
+    .btn{
+   display: inline-block;
+   background: var(--black);
+   margin-top: 1rem;
+   color:var(--white);
+   font-size: 1.7rem;
+   padding:1rem 3rem;
+   cursor: pointer;
+}
+
+.btn:hover{
+   background: var(--main-color);
+   text-align:center;
+}
+.header a{
+  text-decoration:none;
+  font-size:bold;
+  color:black;
+}
+
+   </style>
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header a">
+
+   <a href="index.php" class="logo">travel.</a>
+
+   <nav class="navbar">
+      <a href="index.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+ <div class="heading" style="background:url(img/he3.png) no-repeat">
+ <div class="content">
+            
+               <h1>Travelling Place</h1>
+            </div>
+
+</div>
+
+
+<!-- booking section ends -->
+<!-- footer section starts  -->
+<h1 style="text-align:center; font-size:40px; color:red;  font-style: oblique;
+">Travelling Gallery</h1></br></br>
+<!-- news card1 -->
+<div class="row">
+    <div class="col-sm-3">
+    <div class="card" style="width: 30rem; height:30rem; box-shadow: 20px 20px 50px 10px pink inset;">
+  <img src="img/a.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="font-size:15px; color:red; box-shadow: 20px 20px 50px 10px pink inset;">London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre. .</p>
+    <a href="Links Travel & Tours">click link</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm-3">
+    <div class="card" style="width: 30rem; height:30rem; box-shadow: 20px 20px 50px 10px pink inset;">
+  <img src="img/img.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="font-size:15px; color:red;">London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre..</p>
+    <a href="Links Travel & Tours">click link</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm-3">
+    <div class="card" style="width: 30rem; height:30rem; box-shadow: 20px 20px 50px 10px pink inset;">
+  <img src="img/a.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="font-size:15px; color:red;">London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre.</p>
+    <a href="Links Travel & Tours">click link</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm-3">
+    <div class="card" style="width: 30rem; height:30rem; box-shadow: 20px 20px 50px 10px pink inset;">
+  <img src="img/img.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="font-size:15px; color:red;">London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre...</p>
+    <a href="Links Travel & Tours">click link</a>
+
+  </div>
+</div>
+    </div>
+
+</div>
+<!-- news card2 -->
+
+<div class="row mt-3">
+    <div class="col-sm-3">
+    <div class="card" style="width: 30rem; height:30rem; box-shadow: 20px 20px 50px 10px pink inset;">
+  <img src="img/a.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="font-size:15px;">London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre..</p>
+    <a href="Links Travel & Tours">click link</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm-3">
+    <div class="card" style="width: 30rem; height:30rem; box-shadow: 20px 20px 50px 10px pink inset;">
+  <img src="img/img.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="font-size:15px;">London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre..</p>
+    <a href="Links Travel & Tours">click link</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm-3">
+    <div class="card" style="width: 30rem; height:30rem; box-shadow: 20px 20px 50px 10px pink inset;">
+  <img src="img/a.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="font-size:15px;">London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre..</p>
+    <a href="Links Travel & Tours" class="">click link</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm-3">
+    <div class="card" style="width: 30rem; height:30rem; box-shadow: 20px 20px 50px 10px pink inset;">
+  <img src="img/img.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="font-size:15px;">London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre...</p>
+    <a href="Links Travel & Tours" class="">click link</a>
+  </div>
+</div>
+    </div>
+
+</div>
+<!-- card news3 -->
+<div class="row mt-3">
+    <div class="col-sm-3">
+    <div class="card" style="width: 30rem; height:30rem; box-shadow: 20px 20px 50px 10px pink inset;">
+  <img src="img/a.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="font-size:15px; color:blue;">London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre..</p>
+    <a href="Links Travel & Tours" class="">click link</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm-3">
+    <div class="card" style="width: 30rem; height:30rem; box-shadow: 20px 20px 50px 10px pink inset;">
+  <img src="img/img.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="font-size:15px; color:blue;">London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre..</p>
+    <a href="Links Travel & Tours" class="">click link</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm-3">
+    <div class="card" style="width: 30rem; height:30rem; box-shadow: 20px 20px 50px 10px pink inset;">
+  <img src="img/a.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="font-size:15px; color:blue;">London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre. .</p>
+    <a href="Links Travel & Tours" class="">click link</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm-3">
+    <div class="card" style="width: 30rem; height:30rem; box-shadow: 20px 20px 50px 10px pink inset;">
+  <img src="img/img.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="font-size:15px; color:blue;">London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre..</p>
+    <a href="Links Travel & Tours" class="">click link</a>
+  </div>
+</div>
+    </div>
+
+</div>
+<!-- news card1 -->
+<div class="row mt-3">
+    <div class="col-sm-3">
+    <div class="card" style="width: 30rem; height:30rem; box-shadow: 20px 20px 50px 10px pink inset;">
+  <img src="img/a.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="font-size:15px; color:red; box-shadow: 20px 20px 50px 10px pink inset;">London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre. .</p>
+    <a href="Links Travel & Tours">click link</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm-3">
+    <div class="card" style="width: 30rem; height:30rem; box-shadow: 20px 20px 50px 10px pink inset;">
+  <img src="img/img.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="font-size:15px; color:red;">London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre..</p>
+    <a href="Links Travel & Tours">click link</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm-3">
+    <div class="card" style="width: 30rem; height:30rem; box-shadow: 20px 20px 50px 10px pink inset;">
+  <img src="img/a.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="font-size:15px; color:red;">London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre.</p>
+    <a href="Links Travel & Tours">click link</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm-3">
+    <div class="card" style="width: 30rem; height:30rem; box-shadow: 20px 20px 50px 10px pink inset;">
+  <img src="img/img.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="font-size:15px; color:red;">London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre...</p>
+    <a href="Links Travel & Tours">click link</a>
+
+  </div>
+</div>
+    </div>
+
+</div>
+<!-- news card2 -->
+
+<div class="row mt-3">
+    <div class="col-sm-3">
+    <div class="card" style="width: 30rem; height:30rem; box-shadow: 20px 20px 50px 10px pink inset;">
+  <img src="img/a.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="font-size:15px;">London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre..</p>
+    <a href="Links Travel & Tours">click link</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm-3">
+    <div class="card" style="width: 30rem; height:30rem; box-shadow: 20px 20px 50px 10px pink inset;">
+  <img src="img/img.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="font-size:15px;">London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre..</p>
+    <a href="Links Travel & Tours">click link</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm-3">
+    <div class="card" style="width: 30rem; height:30rem; box-shadow: 20px 20px 50px 10px pink inset;">
+  <img src="img/a.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="font-size:15px;">London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre..</p>
+    <a href="Links Travel & Tours" class="">click link</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm-3">
+    <div class="card" style="width: 30rem; height:30rem; box-shadow: 20px 20px 50px 10px pink inset;">
+  <img src="img/img.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="font-size:15px;">London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre...</p>
+    <a href="Links Travel & Tours" class="">click link</a>
+  </div>
+</div>
+    </div>
+
+</div>
+<!-- card news3 -->
+<div class="row mt-3">
+    <div class="col-sm-3">
+    <div class="card" style="width: 30rem; height:30rem; box-shadow: 20px 20px 50px 10px pink inset;">
+  <img src="img/a.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="font-size:15px; color:blue;">London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre..</p>
+    <a href="Links Travel & Tours" class="">click link</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm-3">
+    <div class="card" style="width: 30rem; height:30rem; box-shadow: 20px 20px 50px 10px pink inset;">
+  <img src="img/img.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="font-size:15px; color:blue;">London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre..</p>
+    <a href="Links Travel & Tours" class="">click link</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm-3">
+    <div class="card" style="width: 30rem; height:30rem; box-shadow: 20px 20px 50px 10px pink inset;">
+  <img src="img/a.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="font-size:15px; color:blue;">London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre. .</p>
+    <a href="Links Travel & Tours" class="">click link</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm-3">
+    <div class="card" style="width: 30rem; height:30rem; box-shadow: 20px 20px 50px 10px pink inset;">
+  <img src="img/img.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="font-size:15px; color:blue;">London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre..</p>
+    <a href="Links Travel & Tours" class="">click link</a>
+  </div>
+</div>
+    </div>
+
+</div>
+
+
+
+<section class="footer mt-5">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="index.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <!-- <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div> -->
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+         <a href="#"> <i class="fas fa-envelope"></i>kirti@123gmail.com</a>
+         <a href="#"> <i class="fas fa-map"></i>india -223456 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>Kirti Pandey</span> | all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="script.js"></script>
+
+</body>
+</html>
